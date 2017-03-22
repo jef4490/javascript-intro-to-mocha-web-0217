41b03537321f4b6e63b7ef65e967705124d92379
jef4490 <jeffrey.hechler@gmail.com>
2017-03-22 07:59:57 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream(cream) {
+  return "I love " + cream
+}
+
+function shouting(str) {
+  return str.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
